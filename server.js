632dbba6409d231e62f126f911c71fe619b5d838
adil tahiri <rajaadil1995@gmail.com>
2022-06-16 02:33:35 +0100

    fix bug

--- server.js
+++ server.js
@@ -9,20 +9,30 @@ require("dotenv").config();
 // Middlewares
 // ========================
 app.set("view engine", "ejs");
-app.use(express.json());
 app.use(express.static("public"));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+let db;
+
 MongoClient.connect(process.env.DB_CONNECT).then((client) => {
-  //   console.log("hello")
-  const db = client.db("test");
-  const travelCollection = db.collection("travels");
+  console.log("hello");
+  db = client.db("test");
+  // let travelCollection = db.collection("travels");
 });
 
 // ========================
 //Router
 // ========================
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  db.collection("travels")
+    .find()
+    .toArray()
+    .then((data) => {
+      console.log(data);
+      res.render("index.ejs", { info: data });
+    })
+    .catch((error) => console.error(error));
 });
 
 app.listen(PORT, () => console.log("is runnig"));
