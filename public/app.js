856140dd98b270531c4eb779810694b53f815f2b
adil tahiri <rajaadil1995@gmail.com>
2022-06-17 02:24:11 +0100

    fetch api

--- public/app.js
+++ public/app.js
@@ -1,10 +1,10 @@
 const country = document.querySelector(".select-country");
 document.querySelector("h2").innerHTML = "hi";
-const url = "https://travel-api-server.herokuapp.com/travel/api/country/";
+const url = "/api";
 fetch(url, {
-  method: "get",
-  mode: "cors",
+  method: "POST",
   headers: { "Content-Type": "application/json" },
+  body: JSON.stringify(info),
 })
   .then((res) => res.json()) // parse response as JSON
   .then((data) => {
